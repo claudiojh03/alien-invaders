d6d495e7d39fd4edcac0747e00131b3f9d6400a7
claudio <claudiojh03@gmail.com>
2022-04-25 22:11:41 -0400

    master

--- game/bullet.py
+++ game/bullet.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    def __inti__(self, ai_game):
+    def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
