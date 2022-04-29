16ac088b2869f195a5b590fe0ed3c56da403341e
claudio <claudiojh03@gmail.com>
2022-04-29 11:05:03 -0400

    master

--- game/bullet.py
+++ game/bullet.py
@@ -1,11 +1,12 @@
 import pygame
 from pygame.sprite import Sprite
+from settings import Settings
 
 class Bullet(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
-        self.settings = ai_game.settings
+        self.settings = Settings
         self.color = self.settings.bullet_color
 
         self.rect = pygame.rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
